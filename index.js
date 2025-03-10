33c6148414419642d9dc18f18224e0c10abef4d2
eduardabelles-flooxmongagua <eduarda.belles.flooxmongagua@gmail.com>
2025-03-10 08:37:16 -0300

    feat: add different URL detection

--- index.js
+++ index.js
@@ -6,7 +6,35 @@ let server = http.createServer((req, res)=>{
 
     console.log('METHOD:', req.method);
 
-    res.end('OK');
+    //res.end('OK');
+
+    switch(req.url){
+        case'/':
+
+        res.statusCode=200;
+        res.setHeader('Content-Type','text/html');
+        res.end('<h1>Olá</h1>');
+
+        break;
+
+        case '/users':
+
+        res.statusCode=200;
+        res.setHeader('Content-Type','aplication/json');
+        res.end(JSON.stringify({
+            users:[{
+              name:'Hcode',
+              email: 'contato@hcode.com.br',
+              id: 1
+            }]
+        }));
+
+        break;
+
+
+     
+
+    }
 });
 
 server.listen(3000,'127.0.0.1', ()=>{
